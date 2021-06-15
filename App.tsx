29eb2f24633d109df149c9e98cb137c8ae349c33
Gál Péter <pepyta118@gmail.com>
2021-06-15 19:23:20 +0200

    chore: setup react redux

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import store from './src/lib/redux/store';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+
+      </Provider>
     </NavigationContainer>
   );
 }
