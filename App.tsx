fb7fbe22ab3ce7576f2443673afaab2e0ccb2611
Gál Péter <pepyta118@gmail.com>
2021-06-15 19:15:34 +0200

    chore: prepare for using screens

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
