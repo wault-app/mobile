c0b630545180b18ae9e9799166d0f60dad7b37c9
Gál Péter <pepyta118@gmail.com>
2021-07-25 19:32:47 +0200

    chore: move bottom sheet modal to `AccountItem` component

--- components/accounts/AccountItem.tsx
+++ components/accounts/AccountItem.tsx
@@ -1,26 +1,64 @@
 import React, { Fragment } from "react";
 import { AccountType } from "@lib/api/Item";
-import { List } from "react-native-paper";
+import { List, Portal } from "react-native-paper";
 import PlatformIcon from "@components/platforms/PlatformIcon";
 import { useNavigation } from "@react-navigation/native";
+import { useMemo } from "react";
+import BottomSheet, { useBottomSheet } from "@components/modal/BottomSheet";
+import { ScrollView } from "react-native-gesture-handler";
+import OpenInBrowserButton from "./OpenInBrowserButton";
+import CopyUsernameButton from "./CopyUsernameButton";
+import CopyPasswordButton from "./CopyPasswordButton";
 
 export type AccountItemProps = {
     account: AccountType;
-    onOpenSheet: () => void;
 };
 
 const AccountItem = (props: AccountItemProps) => {
     const navigation = useNavigation();
+    const snapPoints = useMemo(() => [200, "80%"], []);
+    const sheet = useBottomSheet();
 
-        return (
+    return (
+        <Fragment>
             <List.Item
                 title={props.account.platform}
                 description={props.account.username}
                 onPress={() => navigation.navigate("account-info", { account: props.account })}
-                onLongPress={() => props.onOpenSheet()}
+                onLongPress={() => sheet.current.present()}
                 left={() => <PlatformIcon platform={props.account.platform} />}
             />
-        );
+                
+            <BottomSheet
+                snapPoints={snapPoints}
+                ref={sheet}
+            >
+                <ScrollView>
+                    {!!(props.account?.url || props.account?.platform) && (
+                        <OpenInBrowserButton
+                            url={`https://${props.account.platform}` || props.account.url}
+                        />
+                    )}
+                    {!!props.account?.username && (
+                        <CopyUsernameButton
+                            username={props.account.username}
+                            onCopy={() => {
+                                sheet.current.close();
+                            }}
+                        />
+                    )}
+                    {!!props.account?.password && (
+                        <CopyPasswordButton
+                            password={props.account.password}
+                            onCopy={() => {
+                                sheet.current.close();
+                            }}
+                        />
+                    )}
+                </ScrollView>
+            </BottomSheet>
+        </Fragment>
+    );
 };
 
 export default AccountItem;
--- components/screens/LandingScreen.tsx
+++ components/screens/LandingScreen.tsx
@@ -1,16 +1,9 @@
-import React, { Fragment, useEffect, useMemo, useRef, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import AccountItem from "@components/accounts/AccountItem";
 import { useKeycards } from "@components/providers/DataProvider";
-import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import { Dimensions, RefreshControl } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import { List } from "react-native-paper";
-import { AccountType, ItemType } from "@lib/api/Item";
+import { ItemType } from "@lib/api/Item";
 import { DataProvider, LayoutProvider, RecyclerListView } from "recyclerlistview";
-import CopyUsernameButton from "@components/accounts/CopyUsernameButton";
-import CopyPasswordButton from "@components/accounts/CopyPasswordButton";
-import OpenInBrowserButton from "@components/accounts/OpenInBrowserButton";
-import BottomSheet, { useBottomSheet } from "@components/modal/BottomSheet";
 import CreditCardItem from "@components/cards/CreditCardItem";
 import EmptyItemList from "./LandingScreen/EmptyItemList";
 
@@ -22,11 +15,6 @@ const LandingScreen = (props: LandingScreenProps) => {
     const { keycards, refresh } = useKeycards();
     const [refreshing, setRefreshing] = useState(false);
     const [provider, setProvider] = useState(new DataProvider((r1, r2) => r1 !== r2));
-
-    const [account, setAccount] = useState<AccountType>();
-
-    const sheet = useBottomSheet();
-    const snapPoints = useMemo(() => [200, "80%"], []);
 
     const layoutProvider = new LayoutProvider((index) => 1, (type, dim) => {
         dim.width = width;
@@ -71,10 +59,9 @@ const LandingScreen = (props: LandingScreenProps) => {
                     }}
                     rowRenderer={(index, item: ItemType) => (
                         item.type === "account" ? (
-                            <AccountItem account={item} onOpenSheet={() => {
-                                setAccount(item);
-                                sheet.current.present();
-                            }} />
+                            <AccountItem
+                                account={item}
+                            />
                         ) : item.type === "credit-card" && (
                             <CreditCardItem
                                 creditCard={item}
@@ -87,34 +74,6 @@ const LandingScreen = (props: LandingScreenProps) => {
                     refreshControl={refreshControl}
                 />
             )}
-            <BottomSheet
-                snapPoints={snapPoints}
-                ref={sheet}
-            >
-                <ScrollView>
-                    {!!(account?.url || account?.platform) && (
-                        <OpenInBrowserButton
-                            url={`https://${account.platform}` || account.url}
-                        />
-                    )}
-                    {!!account?.username && (
-                        <CopyUsernameButton
-                            username={account.username}
-                            onCopy={() => {
-                                sheet.current.close();
-                            }}
-                        />
-                    )}
-                    {!!account?.password && (
-                        <CopyPasswordButton
-                            password={account.password}
-                            onCopy={() => {
-                                sheet.current.close();
-                            }}
-                        />
-                    )}
-                </ScrollView>
-            </BottomSheet>
         </Fragment>
     );
 };
