732cf53a2a741437b9bde9d236515b41efd3fed5
Gál Péter <pepyta118@gmail.com>
2021-08-09 17:31:29 +0200

    style: empty home screen icon

--- components/screens/LandingScreen/EmptyItemList.tsx
+++ components/screens/LandingScreen/EmptyItemList.tsx
@@ -1,6 +1,8 @@
+import EmptyCanvas from "@components/illustrations/EmptyCanvas";
 import React from "react";
-import { ScrollViewProps, StyleSheet } from "react-native";
+import { ScrollViewProps, StyleSheet, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import { Text, Title } from "react-native-paper";
 
 export type EmptyItemListProps = ScrollViewProps;
 
@@ -8,14 +10,35 @@ const EmptyItemList = (props: EmptyItemListProps) => {
     return (
         <ScrollView
             {...props}
-            style={[styles.root]}
-        />
+            contentContainerStyle={styles.root}
+        >
+            <View>
+                <EmptyCanvas
+                    width={200}
+                    height={200}
+                />
+            </View>
+            <View>
+                <Title>
+                    No item added yet
+                </Title>
+            </View>
+            <View>
+                <Text>
+                    Create your first item by clicking on the plus icon!
+                </Text>
+            </View>
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     root: {
         flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        alignContent: "center",
+        padding: 16,
     },
 });
 
